be91818113a1aedef7a5421668e1251899bf2871
Eneida Anzora <anzoraeneida@outlook.com>
2025-03-22 11:39:21 -0600

    Agregar archivos de proyecto.

---
+++ LEAM20250324S1.AppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LEAM20250324S1.AppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
